1c2177aac876de9f392c68c08f099f1e59730ebe
Bram van Ballegooijen <99046330@mydavinci.nl>
2023-01-13 10:11:50 +0100

    moved React.js to js folder in laravel

--- resources/js/react.js/bookmarker/src/App.js
+++ resources/js/react.js/bookmarker/src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button path="./resources/views/dashboard.blade.php" name="Home Page"> test </button>   
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
