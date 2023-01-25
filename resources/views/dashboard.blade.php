4b116ede7d9d6bb396af5c55fc6d08626b695b20
Bram van Ballegooijen <99046330@mydavinci.nl>
2023-01-25 10:36:19 +0100

    added fontawesome icons + added list navigation bar and bookmark styling

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,52 @@
 <x-app-layout>
-
+    <!-- import -->
+    <head>
+        <script src="https://kit.fontawesome.com/ac9217cc6c.js" crossorigin="anonymous"></script>
+    </head>
     
+    <!-- list bar -->
     <div style='float: left; border: solid; width: 12%; min-height: 870px; black 1px'>
-        <h1 class='text-center'> mijn lijsten </h1>
+        
+        <!--  team list bar -->
+        <div style='border: solid grey 4px; padding: 10px; margin: 2px;'>
+            <h1> Team list <i class="fa fa-arrow-down"></i> </h1>
+            <p> List 1 </p>
+            <p> List 2 </p>
+            <p> List 3 </p>
+        </div>
+
+        <!-- my list bar -->
+        <div style='border: solid grey 4px; padding: 10px; margin: 2px;'>
+            <h1> My lists <i class="fa fa-arrow-down"></i> </h1>
+            <p> List 1 </p>
+            <p> List 2 </p>
+            <p> List 3 </p>
+        </div>
+
     </div>
 
-    <div class="h-56 grid grid-cols-3 gap-4 content-center">
-        <x-jet-button style='max-width: 10%;'> maak een Bookmarker aan </x-jet-button>
-    </div>
-
+    <!-- bookmarker list -->
     <div style="border: solid; black 1px; margin-left: 25%; margin-right: 25%; min-height: 700px">
-        <h1> voorbeeld mijn bookmarks voor deze lijst </h1>
+        <h1 class='text-center text-2xl' style='border: solid black 1px'> zoekbalk </h1>
+
+        <!-- bookmarker item -->
+        <div style='border: solid grey 4px; padding: 10px; margin: 2px; '>
+            <p class='text-lg'> Bookmarker voorbeeld 1 <i class="fa fa-arrow-down"></i> </p>
+        </div>
+
+        <div style='border: solid grey 4px; padding: 10px; margin: 2px; '>
+            <p class='text-lg'> Bookmarker voorbeeld 2 <i class="fa fa-arrow-down"></i> </p>
+        </div>
+
+        <div style='border: solid grey 4px; padding: 10px; margin: 2px; '>
+            <p class='text-lg'> Bookmarker voorbeeld 3 <i class="fa fa-arrow-down"></i> </p>
+        </div>
+
+        <!-- make bookmarker -->
+        <div class="h-56 grid grid-cols-3 gap-4 content-center">
+            <x-jet-button style='max-width: 20%;'> maak een Bookmarker aan </x-jet-button>
+        </div>
+        
     </div>
 
 </x-app-layout>
