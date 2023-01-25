50826680a73f2f6c07bf9f4f039ebd6861b33df0
Bram van Ballegooijen <99046330@mydavinci.nl>
2023-01-25 09:35:13 +0100

    added begin styling of the homepage

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,16 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
+    
+    <div style='float: left; border: solid; width: 12%; min-height: 870px; black 1px'>
+        <h1 class='text-center'> mijn lijsten </h1>
     </div>
+
+    <div class="h-56 grid grid-cols-3 gap-4 content-center">
+        <x-jet-button style='max-width: 10%;'> maak een Bookmarker aan </x-jet-button>
+    </div>
+
+    <div style="border: solid; black 1px; margin-left: 25%; margin-right: 25%; min-height: 700px">
+        <h1> voorbeeld mijn bookmarks voor deze lijst </h1>
+    </div>
+
 </x-app-layout>
